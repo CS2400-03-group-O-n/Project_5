89d5396eab32622424e394dbc36ac27f878f3b0a
Katie Pham <katiepham@cpp.edu>
2022-12-05 18:57:40 -0800

    created graph for adjacency implementations

--- GraphPackage/Graph.java
+++ GraphPackage/Graph.java
@@ -11,16 +11,6 @@ import ADTPackage.*; // Classes that implement various ADTs
 */
 public class Graph<E> implements BasicGraphInterface<E>
 { 
-  // Constructors
-  // Boolean method 
-  //     isEdge(int source, int target)
-  // Methods:
-  //      addEdge(int source, int target) .
-  //      getLabel(int vertex).
-  //      neighbors(int vertex)
-  //      removeEdge(int source, int target)
-  //      setLabel(int vertex, E newLabel)
-  //      size()
 
   private boolean[][] edges; // edges[i][j] is true if there is a vertex from i to j
   private E[] labels; // labels[i] contains the label for vertex i  
--- GraphTraversalDriver.java
+++ GraphTraversalDriver.java
@@ -12,7 +12,7 @@ public class GraphTraversalDriver
     System.out.println("\nThe graph: {V, E} where\nV = { A, B, C, D, E, F, G, H, I }");
     System.out.println("E = { (A,B), (A, D), (A, E), (B, E), (D, G), (E, F), (E, H), (G, H), (F, C), (F, H), (H, I), (C, B), (I, F) }\n");
 
-    // DirectedGraph<String> BgraphTraversal = new DirectedGraph<>();
+    DirectedGraph<String> BgraphTraversal = new DirectedGraph<>();
     Graph<String> AdjGraph = new Graph<>(10);
   
     AdjGraph.setLabel(1, "A");
@@ -41,36 +41,37 @@ public class GraphTraversalDriver
 
     AdjGraph.getAdjacencyMatrix(AdjGraph);
     AdjGraph.getAdjacencyList(AdjGraph);
-    // BgraphTraversal.addVertex("A");
-    // BgraphTraversal.addVertex("B");
-    // BgraphTraversal.addVertex("C");
+
+    BgraphTraversal.addVertex("A");
+    BgraphTraversal.addVertex("B");
+    BgraphTraversal.addVertex("C");
 
 
-    // BgraphTraversal.addEdge("A", "B", 2.0);
-    // BgraphTraversal.addEdge("B", "C", 3.0);
-    // BgraphTraversal.addEdge("C", "A", 4.0);
-    // BgraphTraversal.addEdge("A", "C", 5.0);
+    BgraphTraversal.addEdge("A", "B", 2.0);
+    BgraphTraversal.addEdge("B", "C", 3.0);
+    BgraphTraversal.addEdge("C", "A", 4.0);
+    BgraphTraversal.addEdge("A", "C", 5.0);
 
 
-    // System.out.println("The breadth-first traversal of the graph: ");
-    // QueueInterface<String> bfs = BgraphTraversal.getBreadthFirstTraversal("A");
-    // while (!bfs.isEmpty())
-    // {
-    //   System.out.print(bfs.dequeue() + " ");
-    // }
-    // System.out.println(); 
+    System.out.println("The breadth-first traversal of the graph: ");
+    QueueInterface<String> bfs = BgraphTraversal.getBreadthFirstTraversal("A");
+    while (!bfs.isEmpty())
+    {
+      System.out.print(bfs.dequeue() + " ");
+    }
+    System.out.println(); 
 
-    // QueueInterface<String> dfs = AdjGraph.getDepthFirstTraversal("A");
-    // System.out.println("The depth-first traversal of the graph:");
+    QueueInterface<String> dfs = AdjGraph.getDepthFirstTraversal("A");
+    System.out.println("The depth-first traversal of the graph:");
     
-    // while (!dfs.isEmpty())
-    // {
-    //   System.out.print(dfs.dequeue() + " ");
-    // }
-    // System.out.println(); 
+    while (!dfs.isEmpty())
+    {
+      System.out.print(dfs.dequeue() + " ");
+    }
+    System.out.println(); 
 
-   // System.out.println("The breadth-first tree: \n");
+   System.out.println("The breadth-first tree: \n");
 
-//    System.out.println("The depth-first tree: ");
+   System.out.println("The depth-first tree: ");
     }
 }
